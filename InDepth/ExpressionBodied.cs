9ba2c6302c8c0f9a9cfbbe870d5cd00363253c45
mohsen <mohsen.amirkhani@live.com>
2022-11-22 09:20:58 +0330

    Expression Bodied and String Format and Alignment

---
+++ InDepth/ExpressionBodied.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InDepth
+{
+    public class ExpressionBodied
+    {
+        public int Prop1 { get; set; }
+
+        public int Prop2 { get; set; }
+
+        // This is Expression Bodied Method, indexer and operation and is not anonymous function
+        public static void GetMyName(string name) => Console.WriteLine($"Hello {name}");
+
+        public static ExpressionBodied operator +(ExpressionBodied i, ExpressionBodied j) => new ExpressionBodied(i.Prop1 + j.Prop1, i.Prop2 + j.Prop2);
+
+        public int this[int index] => Prop1;
+
+
+        
+
+        // C#7 allow constructor and STATIC constructor to be expression-bodied
+        static ExpressionBodied() => Console.WriteLine("Hello From Static Constructor");
+        
+        public ExpressionBodied(int prop1, int prop2)
+        {
+            Prop1 = prop1;
+        }
+
+        public override string ToString()
+        {
+            return $"Prop1: {Prop1}\nProp2: {Prop2}";
+        }
+    }
+
+    public sealed class ReadOnlyListView<T>
+    {
+        private readonly IList<T> _list;
+
+        public ReadOnlyListView(IList<T> list)
+        {
+            _list = list;
+        }
+
+        public T this[int index] => _list[index];
+    }
+}
--- InDepth/Program.cs
+++ InDepth/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Threading.Tasks;
 using InDepth;
 
@@ -58,7 +59,7 @@ namespace delegates
             //10.1.F is of type float but its not convertible to decimal so it calls the object signature of method
             //dt.CallMethods(10.1F);
             //dt.CallMethods(dt);
-            
+
 
 
 
@@ -88,35 +89,104 @@ namespace delegates
 
             /*************** foreach in C# 5 ***************/
 
-            List<string> names = new List<string> { "Mohsen", "Ali", "Mohammad" };
-            var actions = new List<Action>();
-            foreach (var name in names)
-            {
-                // for each iterate over the names in foreach loop a new variable of name would be generated
-                actions.Add(() => Console.WriteLine(name));
-            }
+            //List<string> names = new List<string> { "Mohsen", "Ali", "Mohammad" };
+            //var actions = new List<Action>();
+            //foreach (var name in names)
+            //{
+            //    // for each iterate over the names in foreach loop a new variable of name would be generated
+            //    actions.Add(() => Console.WriteLine(name));
+            //}
 
-            for (int i = 0; i < names.Count - 1; i++)
-            {
-                // take attention to the warning for the n
-                actions.Add(() => Console.WriteLine(names[i]));
-            }
+            //for (int i = 0; i < names.Count - 1; i++)
+            //{
+            //    // take attention to the warning for the n
+            //    actions.Add(() => Console.WriteLine(names[i]));
+            //}
 
-            string n;
-            using (var iterator = names.GetEnumerator())
-            {
-                while (iterator.MoveNext())
-                {
-                    n = iterator.Current;
-                    // take attention to the warning for the n
-                    actions.Add(() => Console.WriteLine(n));
-                }
-            }
+            //string n;
+            //using (var iterator = names.GetEnumerator())
+            //{
+            //    while (iterator.MoveNext())
+            //    {
+            //        n = iterator.Current;
+            //        // take attention to the warning for the n
+            //        actions.Add(() => Console.WriteLine(n));
+            //    }
+            //}
 
-            foreach (var action in actions)
-            {
-                action();
-            }
+            //foreach (var action in actions)
+            //{
+            //    action();
+            //}
+
+
+
+
+
+            /***********************  Expression Bodied  **************************/
+
+            //ExpressionBodied.GetMyName("Mohsen");
+
+            //var exp1 = new ExpressionBodied(1, 2);
+
+            //var exp2 = new ExpressionBodied(3, 4);
+
+            //var result = exp1 + exp2;
+
+            //Console.WriteLine(result);
+            //Console.WriteLine(exp1[1]);
+
+            //var list = new List<int> { 1, 2, 3 };
+            //var listView = new ReadOnlyListView<int>(list);
+
+            //Console.WriteLine(listView[2]);
+
+
+            /************** String Format and Alignment *****************/
+
+            Console.WriteLine("{0, -11}{1}", "----------", "1");
+
+            var price = 95.25m;
+            var tip = price * 0.2m;
+            // 11 points to right from the starting of opening '{'
+            Console.WriteLine("Price: {0,11:C}", price);
+            Console.WriteLine("Tip:   {0,11:C}", tip);
+            Console.WriteLine("Total: {0,11:C}", price + tip);
+
+            Console.WriteLine($"Price: {price,11:C}");
+            Console.WriteLine($"Tip:   {tip,11:C}");
+            Console.WriteLine($"Total: {price + tip,11:C}");
+
+            // Interpolation and verbatim
+            // Interpolation would exactly convert to string.Format in IL
+            Console.WriteLine($@"Price: {price,11:C}
+Tip:   {tip,11:C}
+Total: {price + tip,11:C}");
+
+
+            var culture = CultureInfo.GetCultureInfo("fa-IR");
+            var date = new DateTime(1992, 6, 29);
+            var str = string.Format(CultureInfo.InvariantCulture, "My BirthDay is {0:d}", date);
+            Console.WriteLine(str);
+
+            var cultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
+
+            //foreach (var cultureInfo in cultures)
+            //{
+            //    var priceStr = string.Format(cultureInfo, "Price: {0:C}\n", price);
+            //    Console.WriteLine(priceStr);
+            //    var c = string.Format(cultureInfo, "{0,-15} {1,12:d}", cultureInfo.Name, date);
+            //    Console.WriteLine(c);
+            //}
+            
+
+            var verbatim = @"Hello The ""Date"" is";
+            Console.WriteLine(verbatim.ToString(culture));
+
+            var invariantFormattableString = FormattableString.Invariant(@$"{verbatim} {date}");
+
+            var result = $"The result is {invariantFormattableString}";
+            Console.WriteLine(result);
         }
     }
 }
